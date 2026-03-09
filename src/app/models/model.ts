52c69d46b733fac41ee3a913424f080c44f3cb71
lrPascoal <lucasrodriguespascoal@hotmail.com>
2026-03-09 08:23:06 -0300

    feat: interface de modelo para pet

---
+++ src/app/models/model.ts
@@ -0,0 +1,9 @@
+export interface Pet {
+  id?: number;              // O '?' indica que é opcional (o banco/Java que vai gerar)
+  nome: string;
+  especie: 'Cachorro' | 'Gato' | 'Outro'; // Union type para evitar erros
+  raca: string;
+  peso: number;
+  dataNascimento?: string;  // No início, string facilita a máscara de data
+  tutorId: number;          // Chave estrangeira lógica para o dono
+}
